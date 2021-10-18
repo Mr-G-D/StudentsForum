99114fcf49c74a129382ec72272dd194283e3717
Mr-G-D <daredinesh00@gmail.com>
2021-10-18 14:10:11 +0530

    template modified

--- client/src/index.js
+++ client/src/index.js
@@ -17,22 +17,20 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Test from "./components/frontend/test";
 
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
-import AdminLayout from "layouts/Admin.js";
-import AuthLayout from "layouts/Auth.js";
-
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-      <Redirect from="/" to="/admin/index" />
+      <Route exact path="/">
+        <Test />
+      </Route>
     </Switch>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
