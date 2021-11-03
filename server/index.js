492ba451f6b2fe92a78f1fb4b61168ecc7371b48
Mr-G-D <daredinesh00@gmail.com>
2021-11-03 22:12:51 +0530

    server env file added and integrated

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const test = require("./Models/test");
 const fs = require("fs");
 const csv = require("csv-parser");
 const college = [];
+
+const mongo_username = process.env.MONGO_USERNAME;
+
+const mongo_password = process.env.MONGO_PASSWORD;
+
+const port = process.env.PORT;
 
 app.get("/", (req, res, next) => {
   college.pop();
@@ -19,14 +26,14 @@ app.get("/", (req, res, next) => {
     });
 });
 
-app.listen(3001, async function () {
+app.listen(port, async function () {
   await mongoose.connect(
-    "mongodb+srv://Dinesh:DINESH4046@miniproject.lobyj.mongodb.net/test?retryWrites=true&w=majority",
+    `mongodb+srv://${mongo_username}:${mongo_password}@miniproject.lobyj.mongodb.net/test?retryWrites=true&w=majority`,
     {
       useUnifiedTopology: true,
       useNewUrlparser: true,
     },
   );
   console.log(mongoose.connection.readyState);
-  console.log("server running");
+  console.log("name");
 });
