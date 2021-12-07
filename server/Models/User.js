d9442400249bdf183cb0ab6c2350fc3d9778b638
Mr-G-D <daredinesh00@gmail.com>
2021-12-07 12:56:51 +0530

    user module added

---
+++ server/Models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  emailID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  college: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  courseBegin: {
+    type: Date,
+    required: true,
+  },
+  courseEnd: {
+    type: Date,
+    required: true,
+  },
+});
