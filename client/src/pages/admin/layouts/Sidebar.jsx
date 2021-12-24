ede98c58cb16f823ca1a626cac3ad0b4742e6edd
Mr-G-D <daredinesh00@gmail.com>
2021-12-24 16:02:41 +0530

    sidebar ui completed installed @mui/styles

--- client/src/pages/admin/layouts/Sidebar.jsx
+++ client/src/pages/admin/layouts/Sidebar.jsx
@@ -8,8 +8,10 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { makeStyles } from "@mui/styles";
 
 export default function Sidebar(props) {
+  const classes = useStyles();
   const [state, setState] = useState(false);
 
   const toggleDrawer = (open) => (event) => {
@@ -24,29 +26,45 @@ export default function Sidebar(props) {
 
   return (
     <div>
-      <React.Fragment>
-        <Drawer open={state} onClose={toggleDrawer(false)}>
-          <Box
-            role="presentation"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-          >
-            <List>
-              {["Inbox", "Starred", "Send email", "Drafts"].map(
-                (text, index) => (
-                  <ListItem button key={text}>
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItem>
-                ),
-              )}
-            </List>
-            <Divider />
-          </Box>
-        </Drawer>
-      </React.Fragment>
+      <Drawer
+        className={classes.drawer}
+        open={state}
+        onClose={toggleDrawer(false)}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Box
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+}));
