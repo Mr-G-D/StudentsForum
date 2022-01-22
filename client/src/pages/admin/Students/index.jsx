704ffb2cb0bca1ab4cf3482504b9dbe2456f50e9
Mr-G-D <daredinesh00@gmail.com>
2022-01-22 23:08:34 +0530

    :construction: Users read page set

--- client/src/pages/admin/Students/index.jsx
+++ client/src/pages/admin/Students/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "pages/admin/layouts/Navbar";
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, MenuItem, Select, Typography } from "@mui/material";
 import "styles/admin/students/index.css";
 
 const Students = () => {
@@ -12,6 +12,28 @@ const Students = () => {
         <Typography fontFamily="serif" variant="h4">
           {user}
         </Typography>
+        <Grid className="subtitle" display="flex">
+          <Box className="create-admin">
+            <Button variant="contained">Create New Admin</Button>
+          </Box>
+          <Box className="select-user" display="flex" gap={0.5}>
+            <Typography variant="subtitle1">The List below is of</Typography>
+
+            <Select
+              className="select"
+              variant="standard"
+              value={user}
+              onChange={(e) => {
+                setUser(e.target.value);
+              }}
+              displayEmpty
+              inputProps={{ "aria-label": "Without label" }}
+            >
+              <MenuItem value="Students">Students</MenuItem>
+              <MenuItem value="Admin">Admin</MenuItem>
+            </Select>
+          </Box>
+        </Grid>
       </Box>
     </div>
   );
