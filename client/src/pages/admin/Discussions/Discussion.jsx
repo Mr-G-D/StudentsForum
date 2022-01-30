8f31aa412273194b7b69e045386127f8b5d8a788
Mr-G-D <daredinesh00@gmail.com>
2022-01-30 17:08:29 +0530

    :art: discussions lyout set

--- client/src/pages/admin/Discussions/Discussion.jsx
+++ client/src/pages/admin/Discussions/Discussion.jsx
@@ -1,14 +1,21 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
+import "styles/admin/discussions.css";
 
 const Discussion = (props) => {
   return (
     <Grid className="paper">
-      <Typography>{props.data.title}</Typography>
-      <Typography>{props.data.body}</Typography>
+      <Typography variant="h5" className="discussionTitle">
+        {props.data.title}
+      </Typography>
+      <Typography variant="subtitle2" className="discussionBody">
+        {props.data.body}
+      </Typography>
       <Grid flexDirection="row" justifyContent="space-between" container>
-        <Typography>{props.data.userId}</Typography>
-        <Typography>3 days ago</Typography>
+        <Typography className="discussionAuthor">
+          {props.data.userId}
+        </Typography>
+        <Typography className="discussionTime">3 days ago</Typography>
       </Grid>
     </Grid>
   );
--- client/src/pages/admin/Discussions/index.jsx
+++ client/src/pages/admin/Discussions/index.jsx
@@ -1,17 +1,22 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Navbar from "../layouts/Navbar";
 import Discussion from "./Discussion";
+import "styles/admin/discussions.css";
 
 const Discussions = () => {
   const [data, setData] = useState();
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts",
-      );
-      setData(response.data);
+      const response = await axios.get("https://dummyjson.com/posts");
+      setData(response.data.posts);
     };
     fetchData();
   }, []);
@@ -20,14 +25,37 @@ const Discussions = () => {
       <Navbar />
       <Box className="paper">
         <Typography margin={0} className="activityTitle" variant="subtitle1">
-          Discussions{" "}
+          Discussions
         </Typography>
       </Box>
-      {data
-        ? data.map((ele) => {
-            return <Discussion key={ele.id} data={ele} />;
-          })
-        : ""}
+      <Box className="discussionsMain">
+        <Grid className="paper" flex={1}>
+          <Typography margin={0} className="activityTitle" variant="subtitle1">
+            Filters
+          </Typography>
+          <Box className="filters">
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Label"
+            />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Label"
+            />
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Label"
+            />
+          </Box>
+        </Grid>
+        <Grid flex={5} className="discussions">
+          {data
+            ? data.map((ele) => {
+                return <Discussion key={ele.id} data={ele} />;
+              })
+            : ""}
+        </Grid>
+      </Box>
     </div>
   );
 };
