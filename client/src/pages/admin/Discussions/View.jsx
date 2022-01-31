3038485ee8d339f0777b698b330f85bed5b0eddd
mr-g-d <daredinesh@gmail.com>
2022-01-31 18:59:25 +0530

    :wrench: added rich text editor

--- client/src/pages/admin/Discussions/View.jsx
+++ client/src/pages/admin/Discussions/View.jsx
@@ -4,18 +4,29 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../layouts/Navbar";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import { stateToHTML } from "draft-js-export-html";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const View = () => {
   const params = useParams();
   const [data, setData] = useState();
   const [discussion, setDiscussion] = useState();
-
+  const [text, setText] = useState();
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty(),
+  );
+  const handleChange = (e) => {
+    const contentState = stateToHTML(editorState.getCurrentContent());
+    // JSON.stringify(convertToRaw(editorState.getCurrentContent()))
+    setText(contentState);
+  };
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
         `https://dummyjson.com/posts/${params.id}/comments`,
       );
-      console.log(response.data);
       setData(response.data.comments);
       const result = await axios.get(
         `https://dummyjson.com/posts/${params.id}`,
@@ -47,6 +58,39 @@ const View = () => {
           <CircularProgress />
         )}
         <Box>
+          <Box className="paper">
+            <Typography>Add Comment... </Typography>
+            <Editor
+              placeholder="   Add Comment..."
+              defaultEditorState={editorState}
+              onEditorStateChange={setEditorState}
+              wrapperClassName="wrapper-class"
+              editorClassName="editor-class"
+              toolbarClassName="toolbar-class"
+              onChange={handleChange}
+            />
+          </Box>
+          <Box key={1} className="paper">
+            <Typography display="flex" alignItems="center">
+              <img
+                style={{
+                  padding: "1%",
+                  borderRadius: "50%",
+                }}
+                src={`https://i.pravatar.cc/40?u=1`}
+                alt="Avatar"
+              />
+              Show how to display html data
+            </Typography>
+            <Typography
+              dangerouslySetInnerHTML={{ __html: text }}
+              marginLeft="50px"
+            ></Typography>
+            <Grid display="flex" flexDirection="row" justifyContent="flex-end">
+              <ThumbUpOutlined className="commentIcons" />
+              <ThumbDownOutlined className="commentIcons" />
+            </Grid>
+          </Box>
           {data ? (
             data.map((ele) => {
               return (
